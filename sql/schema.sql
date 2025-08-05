99513473a5f817139f5012bee8744c25ecda5203
gnatykdm <gnatykwork@gmail.com>
2025-08-05 16:32:18 +0200

    schema-update

--- sql/schema.sql
+++ sql/schema.sql
@@ -37,7 +37,8 @@ CREATE TABLE tasks (
     user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     task_name VARCHAR(255) NOT NULL,
     status BOOLEAN NOT NULL DEFAULT FALSE,
-    start_time TIMESTAMP,
+    start_time TIMESTAMP UNIQUE NOT NULL,
+    started BOOLEAN DEFAULT FALSE NOT NULL,
     creation_date TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
